1b990afc4dea2f36c152dc9257a7cec1adf22e2d
Marine PAYET <marine.payet3@gmail.com>
2023-05-13 15:19:23 +0200

    ok connexion

--- config/repositories.php
+++ config/repositories.php
@@ -3,3 +3,4 @@
 include BASE_PATH .'/Repository/ParentRepository.php';
 include BASE_PATH .'/Repository/FlightRepository.php';
 include BASE_PATH. '/Repository/AirportRepository.php';
+include BASE_PATH. '/Repository/UserRepository.php';
