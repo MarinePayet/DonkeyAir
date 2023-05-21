e1299b305a69e005f25e34e02ab0df6eb56fb60a
Marine PAYET <marine.payet3@gmail.com>
2023-05-21 17:54:56 +0200

    remise a zero

--- config/repositories.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include BASE_PATH .'/Repository/ParentRepository.php';
-include BASE_PATH .'/Repository/FlightRepository.php';
-include BASE_PATH. '/Repository/AirportRepository.php';
-include BASE_PATH. '/Repository/UserRepository.php';
